ce28ad011457a7954435edd5941066d1258ad738
saurabhprasadsah <kumarsaurabh2910@gmail.com>
2019-07-04 21:27:08 +0530

    first updation

---
+++ Lecture_1/src2.cpp
@@ -0,0 +1,15 @@
+#include<conio.h>
+#include<iostream>
+using  namespace std;
+int main()
+{
+
+cout<<"enter a number";
+int n;
+cin>>n;
+for(int i=1;i<=10;i++)
+{
+	cout<<n*i<<endl;
+}
+return 0;
+}
---
+++ Lecture_1/src3.cpp
@@ -0,0 +1,11 @@
+#include<conio.h>
+#include<iostream>
+using  namespace std;
+int main()
+{
+	int a;
+	cout<<"enter the number"<<endl;
+	cin>>a;
+	cout<<"the square is "<<a*a<<endl;
+	return 0;
+}
