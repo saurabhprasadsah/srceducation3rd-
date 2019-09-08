aee0bbcfbc2d4204446ed07f7519faa76f4fee07
SAURABH GUPTA <42804047+saurabhprasadsah@users.noreply.github.com>
2019-09-08 07:59:04 +0530

    college cde

---
+++ college code 2.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+#include<stdio.h>
+
+int main()
+{
+	int i,j;
+	for(i=1;i<=4;i++)
+	{
+		for(j=1;j<=i;j++)
+		{
+			cout<<j;
+		}
+		cout<<endl;
+	}
+	for(i=3;i>=1;i--)
+	{
+       for(j=1;j<=i;j++)
+       
+	
+	{
+		cout<<j;
+	}
+   }  
+	cout<<endl;
+	
+	
+	return 0;
+}
