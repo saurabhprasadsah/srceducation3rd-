9089450bc6e9dca24e8611435dfa85feeca70e44
SAURABH GUPTA <42804047+saurabhprasadsah@users.noreply.github.com>
2019-10-02 15:11:26 +0530

    testt

---
+++ test.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{   int a[]={1,2,3,4};
+	
+//	char ch[]={'A','B','C','\0'};
+//	char ch[10]="helloworld";
+//cin>>ch;
+//cout<<ch<<endl;
+//cin.getline(	
+	
+	for(int i=0; ch[i]!='/0';i++)
+	{ cout<<ch[i]<<endl;
+	}
+	cout<<a<<endl;
+	cout<<ch<<endl;
+	
+	return 0;
+}
