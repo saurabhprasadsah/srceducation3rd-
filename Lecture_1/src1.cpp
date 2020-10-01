6d932070019cd428714bdc77d41e4e5f97334249
Gaurav singh <50691771+gauravsingh9891@users.noreply.github.com>
2020-10-01 14:33:12 +0530

    Update src1.cpp

--- Lecture_1/src1.cpp
+++ Lecture_1/src1.cpp
@@ -1,6 +1,6 @@
-#include<iostream>
+#include<iostream>		
 using namespace std;
-int main()
+int main()				// You can also use void in the place of int but remember while using int you must remove "return 0;" .
 {
 	cout<<"hello world"<<endl;
 	return 0;
